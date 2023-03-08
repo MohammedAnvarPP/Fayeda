d2c42a44019b5686205f52ddc5a1516d85cfd142
Yunus <sanuyoonus@gmail.com>
2023-03-08 18:35:29 +0530

    quality section added

--- src/Components/About Us/AboutUs.js
+++ src/Components/About Us/AboutUs.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./About.css";
+import Qualities from "./Qualities";
 
 const AboutUs = () => {
   return (
@@ -19,27 +20,52 @@ const AboutUs = () => {
       </div>
       <section className="about-main-block-two ">
         <div className="container">
-            <div className="row">
-                <div className="image-column">
-                    <div className="image-field">
-                        <img className="image-about" src="https://www.webdevcode.com/html/eastend/version1/images/about/about.jpg" alt="" />
-                    </div>
-                </div>
-                <div className="content-column">
-                    <div className="about-content-block">
-                        <div className="section">
-                            <h2 className="section-heading">
-                                {"over 20 Years Of Experience in Expert Services"}
-                            </h2>
-                            <p>There anyone who loves or pursues nor desires to obtain pain of itself,, bet it is pain, but because occasionally can packages as their default who loves or pursues nor desires to obtain pain of the hellium.</p>
-                        </div>
-                    </div>
-                </div>
+          <div className="row">
+            <div className="image-column">
+              <div className="image-field">
+                <img
+                  className="image-about"
+                  src="https://www.webdevcode.com/html/eastend/version1/images/about/about.jpg"
+                  alt=""
+                />
+              </div>
             </div>
-        
+            <div className="content-column">
+              <div className="about-content-block">
+                <div className="section">
+                  <h2 className="section-heading">
+                    {"over 20 Years Of Experience in Expert Services"}
+                  </h2>
+                  <p>
+                    There anyone who loves or pursues nor desires to obtain pain
+                    of itself,, bet it is pain, but because occasionally can
+                    packages as their default who loves or pursues nor desires
+                    to obtain pain of the hellium.
+                  </p>
+                </div>
+                <button className="btn-contact">Contact U</button>
+              </div>
+            </div>
+          </div>
+          <div className="row" style={{ marginLeft: "8rem" }}>
+            <Qualities
+              icon="https://cdn-icons-png.flaticon.com/512/2491/2491921.png"
+              heading="Our Strategy"
+              description="It is a long established fact that app reader will be distracted by the read won’t be content page."
+            />
+            <Qualities
+              icon="https://cdn-icons-png.flaticon.com/512/826/826070.png"
+              heading="Our Mission"
+              description="It is a long established fact that app reader will be distracted by the read won’t be content page."
+            />
+            <Qualities
+              icon="https://cdn-icons-png.flaticon.com/512/2248/2248315.png"
+              heading="Our Achievements"
+              description="It is a long established fact that app reader will be distracted by the read won’t be content page."
+            />
+          </div>
         </div>
-       
-        </section>
+      </section>
     </div>
   );
 };
