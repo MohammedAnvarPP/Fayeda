623e50eef01c1ecb13148ff5fab6e68f9c6b2021
MohammedAnvarPP <108209368+MohammedAnvarPP@users.noreply.github.com>
2023-03-12 11:35:57 +0530

    Routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import { CountryList } from "./components/CountryList/CountryList";
+import AboutUs from "./components/About Us/AboutUs";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Service from "./components/Services/Service";
 
 function App() {
   return (
     <div className="App">
-      <CountryList />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/AboutUS" element={<AboutUs />} />
+          <Route path="/CountryList" element={<CountryList />} />
+          <Route path="/Service" element={<Service />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
